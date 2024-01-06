bdcc830037ddc97c6a80dd5e0b93271845e1eb95
Duryodhan Majhi <144148242+DuryodhanMajhi@users.noreply.github.com>
2024-01-06 14:45:54 +0530

    scanner class add while_loops.java
    change scanner class

--- while_loops.java
+++ while_loops.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 class while_loops{
+	private static Scanner k;
 	public static void main(String args[]){
 int num;
 System.out.println("Enter number = ");
-Scanner k=new Scanner(System.in);
+ k=new Scanner(System.in);
    num=k.nextInt();
     while(num>0){
     	if(num%2==0){
